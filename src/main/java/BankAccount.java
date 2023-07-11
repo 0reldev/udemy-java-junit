02e22278ed5e39fb978eb17d300102c094057417
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-11 11:10:29 +0200

    Setting JUnit 5

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,26 @@
+public class BankAccount {
+
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public double deposit(double amount, boolean branch) {
+        balance += amount;
+        return balance;
+    }
+
+    public double withdraw(double amount, boolean branch) {
+        balance -= amount;
+        return balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void deposit() {
+        fail("This test has yet to be implemented.");
+    }
+
+    @Test
+    void withdraw() {
+        fail("This test has yet to be implemented.");
+    }
+
+    @Test
+    void getBalance() {
+        fail("This test has yet to be implemented.");
+    }
+}
