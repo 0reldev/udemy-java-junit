35558be07f138105ce78013f39ede60c4bb19991
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-07-11 17:28:11 +0200

    Some assertions

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -6,7 +6,17 @@ class BankAccountTest {
 
     @Test
     void deposit() {
-        fail("This test has yet to be implemented.");
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+        assertEquals(1200, account.getBalance(), 0);
+    }
+
+    @Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+//        assertEquals(true, account.isChecking());
+        assertTrue(account.isChecking());
     }
 
     @Test
@@ -15,7 +25,16 @@ class BankAccountTest {
     }
 
     @Test
-    void getBalance() {
-        fail("This test has yet to be implemented.");
+    void getBalance_deposit() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200, account.getBalance(), 0);
+    }
+
+    @Test
+    void getBalance_withdraw() {
+        BankAccount account = new BankAccount("Tim", "Buchalka", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800, account.getBalance(), 0);
     }
 }
